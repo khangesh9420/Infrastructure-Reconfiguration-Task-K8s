a19084afe8a42b7cf0895f4689b9f1a9d7d45785
Khangesh Matte <72436906+khangesh9420@users.noreply.github.com>
2025-06-11 00:11:22 +0200

    Create update-images.sh

---
+++ update-images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+REGISTRY="khangeshmatte123"
+TAG="$BUILD_ID"
+
+BOOK_FILE="k8s_manifest/book-service.yaml"
+USER_FILE="k8s_manifest/user-service.yaml"
+
+# Update image tags in both manifests
+sed -i "s|docker.io/$REGISTRY/book-service:[^\"']*|docker.io/$REGISTRY/book-service:$TAG|" "$BOOK_FILE"
+sed -i "s|docker.io/$REGISTRY/user-service:[^\"']*|docker.io/$REGISTRY/user-service:$TAG|" "$USER_FILE"
+
+# Git commit & push
+git config --global user.name "Jenkins"
+git config --global user.email "jenkins@example.com"
+
+git add "$BOOK_FILE" "$USER_FILE"
+git commit -m "ci: update image tags to $TAG"
+git push origin main
