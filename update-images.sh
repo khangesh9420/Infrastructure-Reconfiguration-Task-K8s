cff0bacc070f275aef82e624c44428849682eb8b
Khangesh Matte <72436906+khangesh9420@users.noreply.github.com>
2025-06-11 01:18:10 +0200

    Update update-images.sh

--- update-images.sh
+++ update-images.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Check that BUILD_ID is set
 : "${BUILD_ID:?BUILD_ID is not set. Make sure Jenkins sets it.}"
 
 REGISTRY="khangeshmatte123"
@@ -12,8 +11,8 @@ USER_FILE="k8s_manifest/user-deployment.yaml"
 
 echo "Updating image tags in manifests to tag: $TAG"
 
-# Update image tags using sed
-sed -i "s|docker.io/$REGISTRY/book-service:[^\"']*|docker.io/$REGISTRY/book-service:$TAG|" "$BOOK_FILE"
-sed -i "s|docker.io/$REGISTRY/user-service:[^\"']*|docker.io/$REGISTRY/user-service:$TAG|" "$USER_FILE"
+# Simple match and replace
+sed -i "s|docker.io/$REGISTRY/book-service:[^[:space:]]*|docker.io/$REGISTRY/book-service:$TAG|" "$BOOK_FILE"
+sed -i "s|docker.io/$REGISTRY/user-service:[^[:space:]]*|docker.io/$REGISTRY/user-service:$TAG|" "$USER_FILE"
 
 echo "Image tags updated successfully."
